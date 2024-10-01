056e4618d5c21978d40fc90f4a099c7743ee010a
natnael tsegaye <natnaeltsegaye1@gmail.com.com>
2024-10-01 11:39:17 +0300

    tasks for javascript

---
+++ JS/tasks/tasks.js
@@ -0,0 +1,82 @@
+//task #1
+var firstName = 'Natnael';
+const age = 28;
+var student = false;
+
+console.log('first name :'+firstName)
+console.log('age :'+age)
+console.log('are you student?'+' '+student);
+//task #2
+let person={
+    firstname : 'Alex',
+    age : 30,
+    adress :{
+            city : 'AddisAbaba',
+            postalcode : '1000',
+    }
+}
+console.log('first name :'+person.firstname);
+console.log('city :'+person.adress.city);
+//task #3
+let Array= ['apple', 'banana', 'orange', 'grape', 'pear'];
+console.log('second item:'+Array[1]);
+console.log('fourth item:'+Array[3]);
+Array.push('pinapple');
+console.log('recently added item:'+Array[5]);
+Array[6]='Item listed here';
+console.log('Most recently added item:' +Array[6]);
+//task #4
+function printNumbers(n) {
+  for (let a = 0; a <= n; a++) {
+      if (a % 3 === 0 && a % 5 === 0) {
+          console.log("FizzBuzz");
+      } else if (a % 3 === 0) {
+          console.log("Fizz");
+      } else if (a % 5 === 0) {
+          console.log("Buzz");
+      } else {
+          console.log(a);
+      }
+  }
+}
+printNumbers(20);
+//task #5
+let Numbers= [ ];
+console.log('first console log:', Numbers);
+  for (let a = 1; a <= 40; a++) {
+      if (a % 2 === 0 ) {
+          Numbers.push(a);
+      }      
+  }
+console.log('final console log:',Numbers);
+//task #6
+function findnumber() {
+  const target = 20;
+  const array = [1, 22, 13, 14, 85, 96, 67, 80, 97, 120, 19, 2, 3, 4, 15, 16, 6, 8, 9, 20, 44];
+
+  for (let i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+          console.log("Target found at index "+ [i]);
+          break;
+      } else if ((array[i] % 3) === 0) {
+          continue;
+      } else if (i === (array.length - 1) && array[i] != target) {
+          console.log("Target not found");
+          return;
+      }
+  }
+}
+findnumber();
+//task #bonus
+function printPattern(n) {
+  for (let i = 1; i <= n; i++) {
+      let row = '';
+      for (let j = 1; j <= i; j++) {
+          row += '*';
+      }
+      console.log(row);
+  }
+}
+
+// Example usage:
+printPattern(5);
